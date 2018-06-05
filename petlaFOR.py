b158541dd39e371666095047b6efdbbd6b587494
Rafal <36965674+bentkowski@users.noreply.github.com>
2018-06-05 18:59:55 +0200

    pierwszy plik

--- petlaFOR.py
+++ petlaFOR.py
@@ -1,6 +1,6 @@
 import random
 
-for x in range(0,110):
+for x in range(0,100):
     y = (random.randint(0,10))
     if y>5:
         print('wieksze od 5')
@@ -8,3 +8,6 @@ for x in range(0,110):
         print ('mniejsze od 5')
     elif y=5:
         print ('zloty strzal')
+    else:
+        print ('konioec jesty blisko')
+
