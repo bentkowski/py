9939c377f697787bd4f4dc22605aa997acc501fc
rafal.bentkowski <rafal.bentkowski@intive.com>
2018-06-06 11:18:11 +0200

    idiotyczne bledy, == zamiast =

--- petlaFOR.py
+++ petlaFOR.py
@@ -4,10 +4,10 @@ for x in range(0,100):
     y = (random.randint(0,10))
     if y>5:
         print('wieksze od 5')
-    elif y<5:
+    elif y < 5:
         print ('mniejsze od 5')
-    elif y=5:
+    elif y == 5:
         print ('zloty strzal')
     else:
-        print ('konioec jesty blisko')
+        print ('koniec jesty blisko')
 
